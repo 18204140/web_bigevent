0353f6e6f53ad5b9c65d1d89eb0aa1335f8fcbf2
zuozehong <2861549174@qq.com>
2021-09-22 10:45:27 +0800

    实现了登录功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,4 +5,12 @@
 $.ajaxPrefilter(function (options) {
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
     // console.log(options.url);
+
+    // 统一为有权限的接口，设置请求头
+    if (options.url.indexOf('/my/') != -1) {
+        options.headers = { Authorization: localStorage.getItem('token') || '' }
+
+    }
 })
+
+
---
+++ assets/js/index.js
@@ -0,0 +1,50 @@
+$(function () {
+    getuserinfo();
+})
+
+// h获取用户信息
+function getuserinfo() {
+    $.ajax({
+        method: 'get',
+        url: '/my/userinfo',
+        success: function (res) {
+            if (res.status != 0) {
+                localStorage.removeItem('token')
+                location.href = '/day_01/login.html'
+            }
+            // console.log(res);
+            avatar(res.data)
+        },
+
+
+
+    })
+}
+
+// 渲染用户头像
+function avatar(user) {
+    var name = user.username || user.nickname;
+
+    $('#welcome').html('欢迎&nbsp;' + ':' + name)
+    if (user.user_pic == null) {
+        $('.layui-nav-img').hide();
+        var first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show()
+    }
+    else {
+
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+
+    }
+}
+
+// 实现退出功能
+$('.btnLogout').on('click', function () {
+    layui.layer.confirm('确定退出登录吗?', { icon: 3, title: '提示' }, function (index) {
+        //do something
+        localStorage.removeItem('token')
+        location.href = '/day_01/login.html'
+        layer.close(index);
+    });
+})
